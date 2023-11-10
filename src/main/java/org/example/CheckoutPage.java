b4c0190b7cb0092e40d93943ca53303cb280e19f
mkruglov <mrmarkussss@gmail.com>
2023-11-10 06:13:19 +0200

    Create tests for Checkout page

---
+++ src/main/java/org/example/CheckoutPage.java
@@ -0,0 +1,98 @@
+package org.example;
+
+import com.codeborne.selenide.SelenideElement;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CheckoutPage {
+
+    public SelenideElement getCardNumberField(){
+        return $x("//input[@name='card_number']");
+    }
+
+    public SelenideElement getCardTypeDropdown(){
+        return $x("//select[@name='card_type_id']");
+    }
+
+    public SelenideElement getPaymentButton(){
+        return $x("//input[@value='Make Payment']");
+    }
+
+    public void generateNewCards() throws IOException {
+        open("cgi-bin/get_credit_card.py");
+        $x("//input[@value='Generate Credit Card']").click();
+        String cardNumber = $x("/html/body/center/table/tbody/tr/td/center/table/tbody/" +
+                "tr[2]/td[2]/span/b").getText();
+        JSONObject credJson = new JSONObject();
+        credJson.put("visa",cardNumber);
+
+        open("cgi-bin/get_credit_card.py");
+        $x("//select[@name='type']").selectOption(1);
+        $x("//input[@value='Generate Credit Card']").click();
+        credJson.put("mastercard",cardNumber);
+
+        open("cgi-bin/get_credit_card.py");
+        $x("//select[@name='type']").selectOption(2);
+        $x("//input[@value='Generate Credit Card']").click();
+        credJson.put("amex",cardNumber);
+
+
+        FileWriter file = new FileWriter("utils/creditCard.json");
+        file.write(credJson.toJSONString());
+        file.flush();
+    }
+
+    public String getVisaCard() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("utils/creditCard.json"));
+        return ((JSONObject) obj).get("visa").toString();
+
+    }
+
+    public String getMastercardCard() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("utils/creditCard.json"));
+        return ((JSONObject) obj).get("mastercard").toString();
+    }
+
+    public String getAmexCard() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("utils/creditCard.json"));
+        return ((JSONObject) obj).get("amex").toString();
+    }
+
+    public String checkVisaBalance() throws IOException, ParseException {
+        open("cgi-bin/view_balance.py");
+        $x("//input[@name='card_number']").setValue(getVisaCard());
+        $x("//input[@value='Show me the balance']").click();
+        return $x("//h2").getText().replace("$","");
+    }
+
+    public String checkMastercardBalance() throws IOException, ParseException {
+        open("cgi-bin/view_balance.py");
+        $x("//input[@name='card_number']").setValue(getMastercardCard());
+        $x("//input[@value='Show me the balance']").click();
+        return $x("//h2").getText().replace("$","");
+    }
+
+    public String checkAmexBalance() throws IOException, ParseException {
+        open("cgi-bin/view_balance.py");
+        $x("//input[@name='card_number']").setValue(getAmexCard());
+        $x("//input[@value='Show me the balance']").click();
+        return $x("//h2").getText().replace("$","");
+    }
+
+    public boolean makePayment(String cardNumber){
+        getCardNumberField().setValue(cardNumber);
+        getPaymentButton().click();
+        return $x("//*[text()='Thank you for your order!!!']").exists();
+    }
+}
---
+++ src/test/java/org/example/CheckoutPageTest.java
@@ -0,0 +1,107 @@
+package org.example;
+
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CheckoutPageTest extends BaseTest {
+
+    BookPage bookPage = new BookPage();
+    CheckoutPage checkoutPage = new CheckoutPage();
+
+
+    @BeforeClass
+    public void openMainPage() throws IOException, ParseException {
+        checkoutPage.generateNewCards();
+        open("cgi-bin/main.py");
+        UtilitiesSharelane.loginOverrideExpiration();
+        open("cgi-bin/show_book.py?book_id=4");
+        bookPage.addBookToCart();
+        open("cgi-bin/shopping_cart.py");
+        $x("//input[@value='Proceed to Checkout']").click();
+    }
+
+    @BeforeMethod
+    public void openCheckoutPage(){
+        open("cgi-bin/checkout.py?book_id=4&q=1&total=1070.0");
+    }
+
+    @Test(groups = "Smoke",description = "Check card number element on page")
+    public void checkCardNumberField(){
+        Assert.assertTrue(checkoutPage.getCardNumberField().exists());
+    }
+
+    @Test(groups = "Smoke",description = "Check card type dropdown")
+    public void checkCardsDropdown(){
+        Assert.assertTrue(checkoutPage.getCardTypeDropdown().exists());
+    }
+
+    @Test(groups = "Smoke",description = "Check Make Payment button presence")
+    public void checkMakePaymentButtonExists(){
+        Assert.assertTrue(checkoutPage.getPaymentButton().exists());
+    }
+
+    @Test(groups = "Regression", description = "Check invalid Visa credentials payment")
+    public void checkInvalidVisaCredentials(){
+        checkoutPage.getCardTypeDropdown().selectOption(0);
+        Assert.assertFalse(checkoutPage.makePayment("1235321252135212"));
+    }
+
+    @Test(groups = "Regression", description = "Check invalid Mastercard credentials payment")
+    public void checkInvalidMastercardCredentials(){
+        checkoutPage.getCardTypeDropdown().selectOption(1);
+        Assert.assertFalse(checkoutPage.makePayment("1235321252135212"));
+    }
+
+    @Test(groups = "Regression", description = "Check invalid Amex credentials payment")
+    public void checkInvalidAmexCredentials(){
+        checkoutPage.getCardTypeDropdown().selectOption(2);
+        Assert.assertFalse(checkoutPage.makePayment("1235321252135212"));
+    }
+
+    @Test(groups = "Smoke", description = "Check Valid Visa credentials payment")
+    public void checkValidVisaCredentials() throws IOException, ParseException {
+        checkoutPage.getCardTypeDropdown().selectOption(0);
+        Assert.assertTrue(checkoutPage.makePayment(checkoutPage.getVisaCard()));
+    }
+
+    @Test(groups = "Smoke", description = "Check Valid Mastercard credentials payment")
+    public void checkValidMastercardCredentials() throws IOException, ParseException {
+        checkoutPage.getCardTypeDropdown().selectOption(0);
+        Assert.assertTrue(checkoutPage.makePayment(checkoutPage.getMastercardCard()));
+    }
+
+    @Test(groups = "Smoke", description = "Check Valid Amex credentials payment")
+    public void checkValidAmexCredentials() throws IOException, ParseException {
+        checkoutPage.getCardTypeDropdown().selectOption(0);
+        Assert.assertTrue(checkoutPage.makePayment(checkoutPage.getAmexCard()));
+    }
+
+
+    // All 3 tests below fail because of website bugs
+    @Test(groups = "Smoke", description = "Check Visa card balance after payment",
+            priority = 1)
+    public void checkVisaBalanceAfterPayment() throws IOException, ParseException {
+        Assert.assertEquals("990.0", checkoutPage.checkVisaBalance());
+    }
+
+    @Test(groups = "Smoke", description = "Check Mastercard card balance after payment",
+            priority = 1)
+    public void checkMastercardBalanceAfterPayment() throws IOException, ParseException {
+        Assert.assertEquals("990.0", checkoutPage.checkMastercardBalance());
+    }
+
+    @Test(groups = "Smoke", description = "Check Amex card balance after payment",
+            priority = 1)
+    public void checkAmexBalanceAfterPayment() throws IOException, ParseException {
+        Assert.assertEquals("990.0", checkoutPage.checkAmexBalance());
+    }
+
+}
